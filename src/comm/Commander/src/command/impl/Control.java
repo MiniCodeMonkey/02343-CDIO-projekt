6f9d728d54026c1f9ef1772570664516d39f9a9e
terle <terkel.brix@gmail.com>
2011-06-06 09:52:40 +0200

    wer

--- src/comm/Commander/src/command/impl/Control.java
+++ src/comm/Commander/src/command/impl/Control.java
@@ -2,6 +2,7 @@ package command.impl;
 
 import java.io.IOException;
 
+import lejos.nxt.Sound;
 import lejos.nxt.remote.NXTCommand;
 import lejos.nxt.remote.NXTProtocol;
 import command.interfaces.IControl;
@@ -68,7 +69,9 @@ public class Control implements IControl{
 		commander.setOutputState(1, (byte) 0, 0, 0, 0, 0, 0);
 		commander.setOutputState(2, (byte) 0, 0, 0, 0, 0, 0);
 //		System.out.println("STOPPING");
-//		lejos.nxt.Sound.playSoundFile("tires.rso");
+		//lejos.nxt.Sound.playSoundFile("tires.rso");
+		//lejos.nxt.Sound.playSoundFile("Hooray.rso");
+		Sound.playTone(500, 10);
 		setMoving(false);
 	}
 
