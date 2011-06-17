eb6969fe5701c0316c9ffba012ec25860b3a3dd9
hulvei3 <hulvei3@gmail.com>
2011-06-17 02:20:54 -0700

    revert back to before soft start/stop

--- src/comm/CommanderRMI/src/command/impl/Control.java
+++ src/comm/CommanderRMI/src/command/impl/Control.java
@@ -8,13 +8,14 @@ import command.interfaces.IControl;
 import lejos.nxt.Sound;
 import lejos.nxt.remote.NXTCommand;
 import lejos.nxt.remote.NXTProtocol;
+import lejos.nxt.remote.OutputState;
 
 /**
  * @author Morten Hulvej
  *
  */
 public class Control implements IControl, Serializable{
-
+	
 	/**
 	 * 
 	 */
@@ -25,14 +26,14 @@ public class Control implements IControl, Serializable{
 	private boolean inLeftMotion = false;
 	private boolean inRightMotion = false;
 	private boolean stopped = true;
+	
 	private boolean clawMoving;
-
-	private static int sleepTime = 50;
+	
 
 	public Control(NXTCommand commander) {
 		this.commander = commander;
 	}
-
+	
 
 	@Override
 	public void move(int speed, boolean reverse) throws IOException {
@@ -43,82 +44,27 @@ public class Control implements IControl, Serializable{
 		setInRightMotion(false);
 		setInLeftMotion(false);
 		setStopped(false);
-
+		
 		if(reverse){
-			if(speed>=15)softStartBackwards(speed);
 			System.out.println("Moving backwards");
 			commander.setOutputState(0, (byte) speed, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
 			commander.setOutputState(2, (byte) speed, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
 			setInBackwardMotion(true);
 		}else{
-			if(speed>=15)softStartForwards(speed);
 			System.out.println("Moving forwards");
 			commander.setOutputState(0, (byte) -speed, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
 			commander.setOutputState(2, (byte) -speed, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
 			setInForwardMotion(true);
 		}
-
-
-
-
-		//		String print;
-		//		if (reverse)	print="Backwards";
-		//		else	print="Forwards";
-		//		System.out.println("MOVING: "+print);
-
-	}
-
-	private void softStartForwards(int speed) throws IOException{
-		System.out.println("SoftStart Forwards");
-		for(int i =0; i<speed;i++){
-			commander.setOutputState(0, (byte) -i, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
-			commander.setOutputState(2, (byte) -i, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
-			try {
-				Thread.sleep(sleepTime);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	private void softStartBackwards(int speed) throws IOException{
-		System.out.println("SoftStart Backwards");
-		for(int i =0; i<speed;i++){
-			commander.setOutputState(0, (byte) i, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
-			commander.setOutputState(2, (byte) i, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
-			try {
-				Thread.sleep(sleepTime);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	private void softStop() throws IOException{
-		System.out.println("SoftStop");
-		int currentSpeed = commander.getOutputState(0).powerSetpoint;
-		if(isInBackwardMotion()){
-			for(int i=currentSpeed; i==0;i--){
-				commander.setOutputState(0, (byte) i, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
-				commander.setOutputState(1, (byte) i, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
-				commander.setOutputState(2, (byte) i, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);			
-				try {
-					Thread.sleep(sleepTime);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		if(isInForwardMotion()){
-			for(int i=currentSpeed; i==0;i--){
-				commander.setOutputState(0, (byte) -i, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
-				commander.setOutputState(1, (byte) -i, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
-				commander.setOutputState(2, (byte) -i, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);			
-				try {
-					Thread.sleep(sleepTime);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+			
+		
+		
+		
+//		String print;
+//		if (reverse)	print="Backwards";
+//		else	print="Forwards";
+//		System.out.println("MOVING: "+print);
+		
 	}
 
 
@@ -131,10 +77,10 @@ public class Control implements IControl, Serializable{
 		setInRightMotion(false);
 		setStopped(false);
 		System.out.println("moving left");
-
+		
 		commander.setOutputState(0, (byte)turnSpeed, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
 		commander.setOutputState(2, (byte)-turnSpeed, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
-		//		System.out.println("TURNING: left");
+//		System.out.println("TURNING: left");
 		setInLeftMotion(true);
 	}
 
@@ -147,10 +93,10 @@ public class Control implements IControl, Serializable{
 		setInLeftMotion(false);
 		setStopped(false);
 		System.out.println("moving right");
-
+		
 		commander.setOutputState(0, (byte)-turnSpeed, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
 		commander.setOutputState(2, (byte)turnSpeed, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
-		//		System.out.println("TURNING: right");
+//		System.out.println("TURNING: right");
 		setInRightMotion(true);
 	}
 
@@ -158,21 +104,20 @@ public class Control implements IControl, Serializable{
 	public void stop() throws IOException {
 		if (isStopped())
 			return;
-		softStop();
-//		commander.setOutputState(0, (byte) 0, 0, 0, 0, 0, 0);
-//		commander.setOutputState(1, (byte) 0, 0, 0, 0, 0, 0);
-//		commander.setOutputState(2, (byte) 0, 0, 0, 0, 0, 0);
+		commander.setOutputState(0, (byte) 0, 0, 0, 0, 0, 0);
+		commander.setOutputState(1, (byte) 0, 0, 0, 0, 0, 0);
+		commander.setOutputState(2, (byte) 0, 0, 0, 0, 0, 0);
 		System.out.println("stopping");
 
 		setInBackwardMotion(false);
 		setInForwardMotion(false);
 		setInLeftMotion(false);
 		setInRightMotion(false);
-
+		
 		setStopped(true);
 	}
 
-
+	
 
 	/**
 	 * @return the commander
@@ -184,9 +129,9 @@ public class Control implements IControl, Serializable{
 
 	/**
 	 * 
-	 * @return {@code true} hvis robotten er i bevægelse (ikke har fået en {@code stop()} kommando endnu)
+	 * @return {@code true} hvis robotten er i bevÃ¦gelse (ikke har fÃ¥et en {@code stop()} kommando endnu)
 	 * <br>
-	 * {@code false} hvis robotten står stille
+	 * {@code false} hvis robotten stÃ¥r stille
 	 */
 	public boolean isInForwardMotion() {
 		return inForwardMotion;
@@ -244,7 +189,7 @@ public class Control implements IControl, Serializable{
 	@Override
 	public int getBatteryLevel() throws IOException {
 		int milliVolts = commander.getBatteryLevel();
-
+		
 		int result = 9000 - milliVolts;
 		result /= 100;
 		result = 100 - result;
@@ -260,18 +205,18 @@ public class Control implements IControl, Serializable{
 
 	@Override
 	public void openClaw(int clawMotor) throws IOException {
-		//		if (isClawMoving())
-		//			return;
-		//		commander.setOutputState(1, (byte)-clawMotor, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, CLAW_LIMIT);
+//		if (isClawMoving())
+//			return;
+//		commander.setOutputState(1, (byte)-clawMotor, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, CLAW_LIMIT);
 		commander.setOutputState(1, (byte)-clawMotor, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
 		clawMoving = true;
 	}
 
 	@Override
 	public void closeClaw(int clawMotor) throws IOException {
-		//		if (isClawMoving())
-		//			return;
-		//		commander.setOutputState(1, (byte)clawMotor, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, CLAW_LIMIT);
+//		if (isClawMoving())
+//			return;
+//		commander.setOutputState(1, (byte)clawMotor, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, CLAW_LIMIT);
 		commander.setOutputState(1, (byte)clawMotor, NXTProtocol.MOTORON, NXTProtocol.REGULATION_MODE_IDLE, 0, 0, 0);
 		clawMoving = true;
 	}
@@ -287,39 +232,39 @@ public class Control implements IControl, Serializable{
 	public boolean isClawMoving() {
 		return clawMoving;
 	}	
-
+		
 	@Override
 	public void move(boolean reverse) throws IOException {
 		move(DEFAULT_MOVE_SPEED, reverse);
-
+		
 	}
 
 
 	@Override
 	public void left() throws IOException {
 		left(DEFAULT_TURN_SPEED);
-
+		
 	}
 
 
 	@Override
 	public void right() throws IOException {
 		right(DEFAULT_TURN_SPEED);
-
+		
 	}
 
 
 	@Override
 	public void openClaw() throws IOException {
 		openClaw(DEFAULT_CLAW_SPEED);
-
+		
 	}
 
 
 	@Override
 	public void closeClaw() throws IOException {
 		closeClaw(DEFAULT_CLAW_SPEED);
-
+		
 	}
 
 	@Deprecated
@@ -330,30 +275,32 @@ public class Control implements IControl, Serializable{
 			try {
 				Thread.sleep(duration);
 			} catch (InterruptedException e) {
-
+				
 			}
 			stop();
-			//			
-			//			for (int i = 0; i <= duration; i += 700)
-			//			{
-			//				lejos.nxt.Sound.playTone(600, 700);
-			//				Thread.sleep(700);
-			//			}
-
+//			
+//			for (int i = 0; i <= duration; i += 700)
+//			{
+//				lejos.nxt.Sound.playTone(600, 700);
+//				Thread.sleep(700);
+//			}
+			
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-			//		} catch (InterruptedException e) {
-			//			e.printStackTrace();
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
 		}
 	}
 
 
 	@Override
 	public boolean hasCake() {
-
+		
 		Sound.playTone(440, 500);
-
-
+		
+		
 		return false;
 	}
 
